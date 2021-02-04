6ccd837a3da30da2e430f5c2ed1b445146eb6059
Pablo Gavia de Assis Simao <gaviapablo@hotmail.com>
2021-02-03 23:03:47 -0300

    Adição de atributo creditos na classe Materia

--- app/materias/model.py
+++ app/materias/model.py
@@ -6,6 +6,7 @@ class Materia(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     nome = db.Column(db.String(50),nullable=False)
     codigo = db.Column(db.String(15),nullable=False)
+    creditos = db.Column(db.Integer)
 
 
     turmas = db.relationship('Turma',backref='materia')
