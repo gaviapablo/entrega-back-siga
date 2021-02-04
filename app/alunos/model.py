0129efa2bad04bdfa604751bc9d112dbae292f9d
Pablo Gavia de Assis Simao <gaviapablo@hotmail.com>
2021-02-03 22:55:46 -0300

    Adição de DRE como atributo de Aluno

--- app/alunos/model.py
+++ app/alunos/model.py
@@ -3,8 +3,9 @@ from ..association import association_table
 
 class Aluno(db.Model):
     __tablename__ = 'aluno'
-    id = db.Column(db.Integer,primary_key=True,autoincrement=True)
+    id = db.Column(db.Integer,primary_key=True)
     nome = db.Column(db.String(50),nullable=False)
+    dre = db.Column(db.Integer,unique=True)
     cpf = db.Column(db.Integer,unique=True)
     dataNascimento = db.Column(db.String(8),nullable=False)
     sexo = db.Column(db.String(10),nullable=False)
