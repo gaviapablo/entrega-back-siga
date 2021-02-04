59685d9e9461afe43a5b9913c2d250a34fd6e391
Pablo Gavia de Assis Simao <gaviapablo@hotmail.com>
2021-02-03 23:06:52 -0300

    Adição do valor na classe Nota

--- app/notas/model.py
+++ app/notas/model.py
@@ -3,7 +3,7 @@ from ..extensions import db
 class Nota(db.Model):
     __tablename__ = 'nota'
     id = db.Column(db.Integer,primary_key=True)
-
+    valor = db.Column(db.Integer)
     
     boletim_id = db.Column(db.Integer, db.ForeignKey('boletim.id'))
 
